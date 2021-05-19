5e072db7ec5fc1d587cbda06575315c24e7ae3e6
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-05-18 23:21:56 -0300

    Configuring Reactron

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+/* import Routes from './routes'; */
+
+const App = () => {
+  return (
+    <View>
+      <Text>ola</Text>
+    </View>
+  );
+};
+
+export default App;
